c6118609cc6d3241aac7a91b128b0900b61148b8
izac-peterson <izacp@opiniion.com>
2024-12-15 11:28:04 -0700

    fix

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.get("/", async (req, res) => {
   let db = new DatabaseManager();
+  await db.setupDatabase();
   let me = await db.getStudentById("988f8451-a3d2-4011-9769-fb52c68f1457");
 
   res.send(me);
